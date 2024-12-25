da3973e006c995cc4712b401f779fa6ccb056e1a
Triangleman1 <spnewman21@gmail.com>
2024-12-24 19:50:25 -0500

    First working Version

--- src/SNCompany/Patches/MenuManager.cs
+++ src/SNCompany/Patches/MenuManager.cs
@@ -1,47 +1,48 @@
 using System;
-using System.IO;
-using BepInEx;
-using BepInEx.Logging;
 using UnityEngine;
 using UnityEngine.UI;
 using HarmonyLib;
-using Dissonance;
 
-[HarmonyPatch(typeof(MenuManager), "Awake")]
-[HarmonyPostfix]
-[HarmonyPriority(1)]
-public static void Awake_Postfix(MenuManager __instance)
+namespace SNCompany.Patches 
 {
-	//IL_003c: Unknown result type (might be due to invalid IL or missing references)
-	//IL_004b: Unknown result type (might be due to invalid IL or missing references)
-	//IL_00cb: Unknown result type (might be due to invalid IL or missing references)
-	if (__instance.isInitScene)
+	[HarmonyPatch]
+	static class ReplaceLogo 
 	{
-		return;
-	}
-	try
-	{
-		Sprite sprite = Sprite.Create(Plugin.mainLogo, new Rect(0f, 0f, (float)((Texture)Plugin.mainLogo).width, (float)((Texture)Plugin.mainLogo).height), new Vector2(0.5f, 0.5f));
-		GameObject gameObject = ((Component)((Component)__instance).transform.parent).gameObject;
-		Transform val = gameObject.transform.Find("MenuContainer/MainButtons/HeaderImage");
-		//Maybe he meant system.object?
-		if ((UnityEngine.Object)(object)val != (UnityEngine.Object)null)
+		[HarmonyPatch(typeof(MenuManager), "Awake")]
+		[HarmonyPostfix]
+		[HarmonyPriority(0)]
+		public static void MainMenuLogo(MenuManager __instance)
 		{
-			((Component)val).gameObject.GetComponent<Image>().sprite = sprite;
-		}
-		Transform val2 = gameObject.transform.Find("MenuContainer/LoadingScreen");
-		if ((UnityEngine.Object)(object)val2 != (UnityEngine.Object)null)
-		{
-			val2.localScale = new Vector3(1.02f, 1.06f, 1.02f);
-			Transform val3 = val2.Find("Image");
-			if ((UnityEngine.Object)(object)val3 != (UnityEngine.Object)null)
+			if (__instance.isInitScene)
 			{
-				((Component)val3).GetComponent<Image>().sprite = sprite;
+				return;
+			}
+			try
+			{
+				Sprite logoImage = Sprite.Create(Plugin.mainLogo, new Rect(0, 0, Plugin.mainLogo.width, Plugin.mainLogo.height), new Vector2(0.5f, 0.5f));
+                GameObject parent = __instance.transform.parent.gameObject;
+                
+				Transform mainLogo = parent.transform.Find("MenuContainer/MainButtons/HeaderImage");
+                if (mainLogo != null)
+                {
+					mainLogo.gameObject.GetComponent<Image>().sprite = logoImage;
+                }
+
+                Transform loadingScreen = parent.transform.Find("MenuContainer/LoadingScreen");
+                if (loadingScreen != null)
+                {
+                    loadingScreen.localScale = new Vector3(1.02f, 1.06f, 1.02f);
+                    Transform loadingLogo = loadingScreen.Find("Image");
+                    if (loadingLogo != null)
+                    {
+                        loadingLogo.GetComponent<Image>().sprite = logoImage;
+                    }
+                }
+			}
+			catch (Exception ex)
+			{
+				Plugin.Log.LogError((object)ex);
 			}
 		}
-	}
-	catch (Exception ex)
-	{
-		Plugin.Log.LogError((object)ex);
 	}
 }
--- src/SNCompany/Plugin.cs
+++ src/SNCompany/Plugin.cs
@@ -5,73 +5,39 @@ using BepInEx.Logging;
 using UnityEngine;
 using HarmonyLib;
 
-/*
-  Here are some basic resources on code style and naming conventions to help
-  you in your first CSharp plugin!
-
-  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-  https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-  https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-*/
-
-[BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+namespace SNCompany
 {
-  public static ManualLogSource Log = null!;
-  public static Texture2D mainLogo;
-
-  private void Awake()
-  {
-    /*
-      BepinEx makes you a ManualLogSource for free called "Logger"
-      and I created a static value above to hold on to it so other
-      parts of your plugin's code can find it by using Plugin.Log
-
-      We assign it here
-    */
-    Log = Logger;
-    Harmony val = new Harmony(LCMPluginInfo.PLUGIN_GUID);
-	try
+	[BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
 	{
-		val.PatchAll();
-	}	
-	catch (Exception ex)
-	{
-		Log.LogError((object)("Failed to patch: " + ex));
-	}
-
-    // Log our awake here so we can see it in LogOutput.txt file
-    Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} is loaded!");
-
-	/*
-    AssetBundle bundle = BundleUtilities.LoadBundleFromInternalAssembly("morecompany.assets", Assembly.GetExecutingAssembly());
-    string name = "assets/morecompanyassets/morecompanytransparentred.png";
-		if (Object.op_Implicit((Object)(object)bundle))
+		public static ManualLogSource Log = null!;
+		public static Texture2D mainLogo = null!;
+		private void Awake()
 		{
-			Object val = bundle.LoadAsset(name);
-			if (val != (Object)null)
+			Log = Logger;
+			Harmony val = new Harmony(LCMPluginInfo.PLUGIN_GUID);
+
+			AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location), "sncompany.assetbundle"));
+			if (assetBundle != null) 
 			{
-				val.hideFlags= (HideFlags)32;
-				mainLogo= (Texture2D)(object)val;
+				mainLogo = assetBundle.LoadAsset<Texture2D>("ShawnNewman.png");
 			}
-			else mainLogo = default(Texture2D);
-			bundle.Unload(false);
+			else 
+			{
+				Log.LogError("Failed to load bundle");
+			}
+
+			try
+			{
+				val.PatchAll();
+				
+			}	
+			catch (Exception ex)
+			{
+				Log.LogError((object)("Failed to patch: " + ex));
+			}
+
+			Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} is loaded!");
 		}
-	}*/
-	
-	/*string name = "assets/morecompanyassets/morecompanytransparentred.png";
-	mainLogo = (Texture2D) Resources.Load(name);
-	*/
-
-	AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(((BaseUnityPlugin)this).Info.Location), "SNCompanyBundle"));
-	if (assetBundle != null) 
-	{
-		mainLogo = assetBundle.LoadAsset<Texture2D>("SNCompany.png");
 	}
-	else 
-	{
-		Log.LogError("Failed to load bundle");
-	}
-  }
-
 }
