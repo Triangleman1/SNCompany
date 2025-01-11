ea75d1a555413c76ab37635a6255541736b42144
Triangleman1 <spnewman21@gmail.com>
2025-01-11 05:22:10 -0500

    Made penalty text more characteristic of the company

--- src/SNCompany/Patches/Penalties.cs
+++ src/SNCompany/Patches/Penalties.cs
@@ -7,7 +7,7 @@ namespace SNCompany.Patches
 		[HarmonyPatch(typeof(HUDManager), "ApplyPenalty")]
 		[HarmonyPrefix]
 		public static bool RemovePenalty() {
-			HUDManager.Instance.statsUIElements.penaltyAddition.text = "You guys suck\nStop dying";
+			HUDManager.Instance.statsUIElements.penaltyAddition.text = "COMPANY PROPERTY WAS LOST\nYOU ARE UNSATISFACTORY ASSETS";
     		HUDManager.Instance.statsUIElements.penaltyTotal.text = "DUE: 0";
 			Plugin.Log.LogInfo($"Removed Fine");
 			return false;
